f216cf8bb04e9bea4db5338da517640a4ad1673c
Killer_Quinn <51754303+certseeds@users.noreply.github.com>
2020-10-02 19:54:49 +0800

    Branch: project1. finish part 3.4's work.
    Signed-off-by: Killer_Quinn <51754303+Certseeds@users.noreply.github.com>

--- project1/judge.sh
+++ project1/judge.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -eoux pipefail
 ###
- # @Github: https://github.com/Certseeds/CS323_Compilers_2020F
- # @Organization: SUSTech
- # @Author: nanoseeds
- # @Date: 2020-09-19 17:59:07
- # @LastEditors: nanoseeds
- # @LastEditTime: 2020-09-19 19:16:11
-### 
+# @Github: https://github.com/Certseeds/CS323_Compilers_2020F
+# @Organization: SUSTech
+# @Author: nanoseeds
+# @Date: 2020-09-19 17:59:07
+# @LastEditors: nanoseeds
+# @LastEditTime: 2020-10-02 19:50:45
+###
 CMAKE_DIR="cmake-build-debug"
 cmake_ensure_dir() {
   if [[ ! -d "${CMAKE_DIR}" ]]; then
@@ -17,11 +17,13 @@ cmake_ensure_dir() {
 compiler() {
   cd "${CMAKE_DIR}"
   cmake ..
-  make -j "$(nproc)"
+  make
+  #-j "$(nproc)"
   cd ..
 }
 run_test() {
-   ./${CMAKE_DIR}/Flex.out ./test/testcase1
+  ./${CMAKE_DIR}/Flex.out ./test/testcase1.in >./test/testcase1.test.out
+  diff ./test/testcase1.test.out ./test/testcase1.data.out -y --left-column
 }
 cmake_ensure_dir
 compiler
