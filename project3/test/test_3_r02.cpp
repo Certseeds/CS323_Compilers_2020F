05f727efa1bee9bb9cd6d7a3c4acbab7ea090bd7
Certseeds <51754303+Certseeds@users.noreply.github.com>
2020-12-22 16:48:27 +0800

    Branch: project3, working for finish all the test cases, now it is 8/10,'01,02,03,04,06,08,09,10'
    Signed-off-by: Killer_Quinn <51754303+Certseeds@users.noreply.github.com>

--- project3/test/test_3_r02.cpp
+++ project3/test/test_3_r02.cpp
@@ -7,35 +7,35 @@ int main() {
     if (year == (year / 100 * 100)) {
         if (year == (year / 400 * 400)) {
             if (month == 2) {
-                write(29);
+                write(29);// 2000,{2}
             } else if (month == (month / 2 * 2)) {
-                write(30);
+                write(30);// 2000,{4,6,8,10,12}
             } else
-                write(31);
+                write(31);//2000,{1,3,5,7,9,11}
         } else {
             if (month == 2) {
-                write(28);
+                write(28);// 1900,{2}
             } else if (month == (month / 2 * 2)) {
-                write(30);
+                write(30);//1900,{4,6,8,10,12}
             } else {
-                write(31);
+                write(31);//1900,{1,3,5,7,9,11}
             }
         }
     } else if (year == (year / 4 * 4)) {
         if (month == 2) {
-            write(29);
+            write(29);//1984,{2}
         } else if (month == (month / 2 * 2)) {
-            write(30);
+            write(30);//1984,{4,6,8,10,12}
         } else {
-            write(31);
+            write(31);//1984,{1,3,5,7,9,11}
         }
     } else {
         if (month == 2) {
-            write(28);
+            write(28);//1919,{2}
         } else if (month == (month / 2 * 2)) {
-            write(30);
+            write(30);//1919,{4,6,8,10,12}
         } else {
-            write(31);
+            write(31);//1919,{1,3,5,7,9,11}
         }
     }
     return 0;
